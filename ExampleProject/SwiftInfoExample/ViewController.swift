ee23bb2d95f254218d439cb3c2a200709cdd3ff3
Bruno Rocha <brunorochaesilva@gmail.com>
2019-04-30 09:36:12 -0300

    Example project

---
+++ ExampleProject/SwiftInfoExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftInfoExample
+//
+//  Created by Bruno Rocha on 4/30/19.
+//  Copyright © 2019 rockbruno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
