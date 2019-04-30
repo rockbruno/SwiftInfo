128c39591d06c7a1625d69c9c82008f6fc48b5f5
Bruno Rocha <brunorochaesilva@gmail.com>
2019-04-30 09:37:48 -0300

    Example Project

--- ExampleProject/SwiftInfoExample/ViewController.swift
+++ ExampleProject/SwiftInfoExample/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
     }
 
 
