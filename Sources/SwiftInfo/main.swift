ab3822d1af832496178c7113a422f87e423ad191
Bruno Rocha <brunorochaesilva@gmail.com>
2020-01-13 13:10:13 -0300

    Bump version

--- Sources/SwiftInfo/main.swift
+++ Sources/SwiftInfo/main.swift
@@ -7,7 +7,7 @@ public struct Main {
     static func run() {
         let fileUtils = FileUtils()
         let toolchainPath = getToolchainPath()
-        log("SwiftInfo 2.3.6")
+        log("SwiftInfo 2.3.8")
         if ProcessInfo.processInfo.arguments.contains("-version") {
             exit(0)
         }
