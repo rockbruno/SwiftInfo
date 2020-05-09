9a0744025fd9edb7b3720e6bf5b020109bdf3ee9
Bruno Rocha <rochab@spotify.com>
2020-05-09 17:12:58 +0200

    Ver bump

--- Formula/swiftinfo.rb
+++ Formula/swiftinfo.rb
@@ -1,11 +1,11 @@
 class Swiftinfo < Formula
   desc "📊 Extract and analyze the evolution of an iOS app's code."
   homepage "https://github.com/rockbruno/SwiftInfo"
-  version "2.3.8"
+  version "2.3.10"
   url "https://github.com/rockbruno/SwiftInfo/releases/download/#{version}/swiftinfo.zip"
   # TODO: Try something to provide a SHA automatically
 
-  depends_on :xcode => ["11.3", :build]
+  depends_on :xcode => ["11.4", :build]
 
   def install
     bin.install Dir["bin/*"]
