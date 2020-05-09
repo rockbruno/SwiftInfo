3ea7491e83f09bba84dfd318c1d3bd27ce785a3c
Bruno Rocha <rochab@spotify.com>
2020-05-09 17:44:54 +0200

    Update example project

--- ExampleProject/_CDToMeAndRun.sh
+++
@@ -1,6 +0,0 @@
-bundle install
-bundle exec pod install
-bundle exec fastlane beta
-./Pods/SwiftInfo/bin/swiftinfo
-echo "-------"
-echo "This bash script runs SwiftInfo outside of fastlane so you can see the output, but check out the Fastfile to see how you could use this in a real project."
