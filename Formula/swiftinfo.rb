69b27d34e14bd28b8d2c7980e58dd933f7752661
Maksym Prokopchuk <rowwingman@gmail.com>
2020-10-12 16:09:05 +0200

    [brew] Update formula to xcode 12 and version 2.3.12

--- Formula/swiftinfo.rb
+++ Formula/swiftinfo.rb
@@ -1,11 +1,11 @@
 class Swiftinfo < Formula
   desc "📊 Extract and analyze the evolution of an iOS app's code."
   homepage "https://github.com/rockbruno/SwiftInfo"
-  version "2.3.11"
+  version "2.3.12"
   url "https://github.com/rockbruno/SwiftInfo/releases/download/#{version}/swiftinfo.zip"
   # TODO: Try something to provide a SHA automatically
 
-  depends_on :xcode => ["11.4", :build]
+  depends_on :xcode => ["12.0", :build]
 
   def install
     bin.install Dir["bin/*"]
